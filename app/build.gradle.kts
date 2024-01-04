97d44b5a0971a9f79e1254c4138c466e79e6f685
pacjo <kamil.kroliczek2004@gmail.com>
2024-01-04 20:50:53 +0100

    main: remove navigation-compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -114,7 +114,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.navigation:navigation-compose:2.7.6")
 
     "genericweatherImplementation"("com.google.code.gson:gson:2.10.1")
 }
