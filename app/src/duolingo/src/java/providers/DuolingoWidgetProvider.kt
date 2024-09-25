75c0ae8d4ccc9379071fcfd19dd165380bebcd57
pacjo <kamil.kroliczek2004@gmail.com>
2024-09-25 15:01:51 +0200

    (duolingo): fix blurred image after duolingo update

--- app/src/duolingo/src/java/providers/DuolingoWidgetProvider.kt
+++ app/src/duolingo/src/java/providers/DuolingoWidgetProvider.kt
@@ -4,8 +4,6 @@ import android.appwidget.AppWidgetProviderInfo
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.RemoteViews
 import android.widget.TextView
 import com.kieronquinn.app.smartspacer.sdk.provider.SmartspacerTargetProvider
@@ -25,62 +23,44 @@ class DuolingoWidgetProvider: SmartspacerWidgetProvider() {
         const val PACKAGE_NAME = "com.duolingo"
         private const val PROVIDER_CLASS = "com.duolingo.streak.streakWidget.StreakWidgetProvider"
 
-        // The following code needs some explanation
-        //   - the view object we get is roughly square and we need it to be rectangular with ~16:9 ratio
-        //   - the background is a gradient
-        //   - no matter what I do, I can't get the widget in other proportions
-        // so we (and by that I mean "I"):
-        //   1. make a canvas that's (16/9)*height of the original view (since at a glance tries to fit image vertically-first)
-        //   2. get background drawable and resize it to the new size
-        //   3. remove background from original view
-        //   4. draw both background and background-less views onto canvas
-        //   5. save the result so it can be used later
-        // sound simple, looks nice, took way to long
-
         fun saveWidgetViewToFile(view: View, file: File) {
-            // without this we get empty image
-            view.measure(0, 0)  // those don't matter
+            // get some starting size
+            view.measure(
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+            )
             view.layout(0, 0, view.measuredWidth, view.measuredHeight)
 
-            // this will be out result bitmap
+            // recalculate size in 16x9 ratio
+            // (recalculated from width since at the time of writing duolingo gives us a 3px high widget)
+            val recalculatedWidth = view.width
+            val recalculatedHeight = (view.width * (9f/16f)).toInt()
+
             val extendedBitmap = Bitmap.createBitmap(
-                (view.measuredHeight * (16/9F)).toInt(),
-                view.measuredHeight,
+                recalculatedWidth,
+                recalculatedHeight,
                 Bitmap.Config.ARGB_8888
             )
 
             val extendedCanvas = Canvas(extendedBitmap)
 
-            val backgroundIDs = listOf(
-                    "background",
-                    "backgroundExtraCenterCrop",
-                    "backgroundExtraCenterFit",
-                    "backgroundExtraLeft"
+            view.run {
+                // resize
+                measure(
+                    View.MeasureSpec.makeMeasureSpec(recalculatedWidth, View.MeasureSpec.EXACTLY),
+                    View.MeasureSpec.makeMeasureSpec(recalculatedHeight, View.MeasureSpec.EXACTLY)
                 )
 
-            // stretch all backgrounds
-            for (backgroundID in backgroundIDs) {
-
-                // get background with id and layout it (to change its size)
-                val backgroundView = view.findViewByIdentifier<ImageView>("$PACKAGE_NAME:id/$backgroundID")
-                backgroundView?.layout(
+                layout(
                     0,
                     0,
-                    (view.measuredHeight * (16/9F)).toInt(),
-                    view.measuredHeight
+                    recalculatedWidth,
+                    recalculatedHeight
                 )
 
-                // remove background from widget view
-                (view as ViewGroup).removeView(view.findViewByIdentifier<ImageView>("$PACKAGE_NAME:id/$backgroundID"))
-
-                // draw (now resized) background onto canvas
-                backgroundView?.draw(extendedCanvas)
-
+                // and draw
+                draw(extendedCanvas)
             }
-
-            // foreground (original view without backgrounds)
-            extendedCanvas.translate((((view.measuredHeight * (16/9F)) - view.measuredWidth))/2, 0F)
-            view.draw(extendedCanvas)        // paint the parent with background removed
 
             // Save the bitmap to a file
             FileOutputStream(file).use { out ->
