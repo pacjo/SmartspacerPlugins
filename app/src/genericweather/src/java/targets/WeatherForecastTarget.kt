4ae845b400fd99a5726fb7036c81cf246a7a41de
pacjo <kamil.kroliczek2004@gmail.com>
2024-10-22 22:16:02 +0200

    (genericWeather): change target wording from "hotter" to "warmer"
    while definitely not as hot as the last one, it's much more appropriate. thanks sister

--- app/src/genericweather/src/java/targets/WeatherForecastTarget.kt
+++ app/src/genericweather/src/java/targets/WeatherForecastTarget.kt
@@ -139,7 +139,7 @@ class WeatherForecastTarget: SmartspacerTargetProvider() {
                     // temperature diff can be negative, so we take away the minus just in case,
                     // TODO: there's for sure a better way to write this
                     temperatureDiff.temperature!! < 0 -> "${temperatureDiff.toString().replace("-", "")} colder then"
-                    temperatureDiff.temperature!! > 0 -> "${temperatureDiff.toString().replace("-", "")} hotter then"
+                    temperatureDiff.temperature!! > 0 -> "${temperatureDiff.toString().replace("-", "")} warmer then"
 
                     else -> "the same as"
                 }
