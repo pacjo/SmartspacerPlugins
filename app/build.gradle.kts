c7897024d6547846906d4d64f8f34197653c4d99
pacjo <kamil.kroliczek2004@gmail.com>
2025-07-07 16:56:39 +0200

    (all): update dependencies, bump target and compile SDK to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "nodomain.pacjo.smartspacer.plugin"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "nodomain.pacjo.smartspacer.plugin"
         minSdk = 29
-        targetSdk = 35
+        targetSdk = 36
 
         // 1000 * major + 100 * minor + patch (assigned randomly to force patch updates in manager)
         versionCode = 1600
@@ -127,15 +127,15 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("androidx.compose.foundation:foundation-android:1.8.0")
+    implementation("androidx.compose.foundation:foundation-android:1.8.3")
     implementation("androidx.compose.material3:material3:1.3.2")
     implementation("com.kieronquinn.smartspacer:sdk-plugin:1.1")
-    implementation(platform("androidx.compose:compose-bom:2025.04.01"))
+    implementation(platform("androidx.compose:compose-bom:2025.06.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.datastore:datastore-preferences:1.2.0-alpha01")
+    implementation("androidx.datastore:datastore-preferences:1.2.0-alpha02")
 
     implementation("com.mikepenz:iconics-core:5.5.0-compose01")
     implementation("com.mikepenz:iconics-compose:5.5.0-compose01")
@@ -144,8 +144,8 @@ dependencies {
 
     "genericweatherImplementation"("com.google.code.gson:gson:2.13.1")
 
-    "localbatteryImplementation"("androidx.datastore:datastore:1.1.5")
-    "localbatteryImplementation"("com.google.protobuf:protobuf-javalite:4.30.2")
+    "localbatteryImplementation"("androidx.datastore:datastore:1.1.7")
+    "localbatteryImplementation"("com.google.protobuf:protobuf-javalite:4.31.1")
 }
 
 protobuf {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.9.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
-    id("com.google.protobuf") version "0.9.4" apply false
+    id("com.android.application") version "8.11.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
+    id("com.google.protobuf") version "0.9.5" apply false
 }
